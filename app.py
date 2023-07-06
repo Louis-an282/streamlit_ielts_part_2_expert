da3c994f24b3a5ffe0b69196bad70cbd5a8ac7a3
Quang Nguyen Dang <quangnguyendang@Quangs-MacBook-Air.local>
2023-07-06 09:11:54 +0700

    tested another azure method

--- app.py
+++ app.py
@@ -1,52 +1,64 @@
 
 import os
-
+import requests
 import streamlit as st
 from dotenv import load_dotenv
 from langchain import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
-import azure.cognitiveservices.speech as speechsdk
 
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 # eleven_api_key = os.getenv("ELEVEN_API_KEY")
 
-llm = ChatOpenAI(temperature=1.2, model="gpt-3.5-turbo-0613")
+llm = ChatOpenAI(temperature=1.2, model="gpt-3.5-turbo")
 
+def get_azure_access_token():
+    azure_key = os.environ.get("AZURE_SUBSCRIPTION_KEY")
+    try:
+        response = requests.post(
+            "https://southeastasia.api.cognitive.microsoft.com/sts/v1.0/issuetoken",
+            headers={
+                "Ocp-Apim-Subscription-Key": azure_key
+            }
+        )
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error: {e}")
+        return None
 
-# def generate_answer(card, topic):
-#     """Generate an answer using the langchain library and OpenAI's GPT-3 model."""
-#     prompt = PromptTemplate(
-#         input_variables=["card", "topic"],
-#         template=""" 
-#          You are an 18-year-old girl who is attending an English test. 
-#          Answer the IELTS Speaking Part 2 task card in 200 words using the chosen topic. Use a conversational tone but not too casual. The vocabulary should be that of a high school student.
-#          Avoid formality and avoid written English such as furthermore, therefore, overall, and in conclusion. 
-#          Here is the task card: {card}. And here's the chosen topic: {topic}
-#                  """
-#     )
-#     chain = LLMChain(llm=llm, prompt=prompt)
-#     return chain.run({
-#     'card': card,
-#     'topic': topic
-#     })
+    return response.text
 
-def text_to_speech(text):
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('AZURE_SUBSCRIPTION_KEY'), region=os.environ.get('SPEECH_REGION'))
-    audio_config = speechsdk.audio.AudioOutputConfig(filename="audio.wav")
+def text_to_speech(text, voice_name='en-US-AriaNeural'):
+    access_token = get_azure_access_token()
 
-    # The language of the voice that speaks.
-    speech_config.speech_synthesis_voice_name='en-US-SaraNeural'
+    if not access_token:
+        return None
 
-    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    try:
+        response = requests.post(
+            "https://southeastasia.tts.speech.microsoft.com/cognitiveservices/v1",
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Content-Type": "application/ssml+xml",
+                "X-MICROSOFT-OutputFormat": "riff-24khz-16bit-mono-pcm",
+                "User-Agent": "TextToSpeechApp",
+            },
+            data=f"""
+                <speak version='1.0' xml:lang='en-US'>
+                <voice name='{voice_name}'>
+                    {text}
+                </voice>
+                </speak>
+            """,
+        )
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error: {e}")
+        return None
 
-    # Synthesize the text to the default speaker.
-    speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
-    
-    return speech_synthesis_result
+    return response.content
 
 def app():
     st.title("IELTS Speaking Part 2 Expert")
@@ -78,12 +90,13 @@ def app():
                 prompt_template = PromptTemplate(input_variables=["answer_text"], template=template)
                 collocation_chain = LLMChain(llm=llm, prompt=prompt_template)
                 collocations = collocation_chain.run(answer_text)
-                audio = text_to_speech(answer_text)
-                audio_data = audio.audio_data
-            st.success(answer_text)
-            st.audio(audio_data, format='audio/wav')
-            st.markdown(collocations)
-
+                st.success(answer_text)
+            with st.spinner('Generating audio and extracting collocations...'):
+            # voice_name = st.selectbox("Select a voice:", ['en-US-AriaNeural', 'en-US-GuyNeural', 'en-GB-RyanNeural'])
+                speech = text_to_speech(answer_text, 'en-US-AriaNeural')
+                st.audio(speech, format='wav')
+                st.header("Collocations")
+                st.markdown(collocations)
 
 if __name__ == '__main__':
     app()
